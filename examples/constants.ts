ef94ffbd557a411c65c11c73608946fcf33e4689
adarshzpatel <adarshzpatel@gmail.com>
2024-04-10 17:39:46 +0530

    update program id

--- examples/constants.ts
+++ examples/constants.ts
@@ -1,6 +1,6 @@
 
 export const rpcUrl =
   "http://api.devnet.solana.com";
-export const programId = "DVYGTDbAJVTaXyUksSwAwZr3rw5HmKZsATm6EmSenQAq";
+export const programId = "DLisWw99mbFRajC9aLCk1kE9xBLVTQjvkGy7i6q9PpfD";
 export const marketPda = "2to6E69S1cukMn53zpTGcHCNmvKoH8mFNfT2xRkJMszL";
 
